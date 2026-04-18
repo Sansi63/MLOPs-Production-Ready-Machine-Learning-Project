1b9e929138c4d354bc07fc91a5663dba0b29f02a
Sansi63 <sinhasanket160@gmail.com>
2026-04-19 00:21:23 +0530

    data ingestion added

--- demo.py
+++ demo.py
@@ -1,9 +1,4 @@
-from us_visa.logger import logging
-from us_visa.exception import USvisaException
-import sys
-logging.info("welcome to logging")
-try:
-    2/0
-except Exception as e:
-    raise USvisaException(e,sys)
-        
+from us_visa.pipline.training_pipeline import TrainPipeline
+obj = TrainPipeline()
+obj.run_pipeline()
+
