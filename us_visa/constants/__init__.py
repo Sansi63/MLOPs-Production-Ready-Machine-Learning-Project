a7cbeb7259ff244419e47da72775e894b764b8aa
Sansi63 <sinhasanket160@gmail.com>
2026-04-22 00:40:42 +0530

    added data validation

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -1,3 +1,6 @@
+import os
+from datetime import date 
+
 DATABASE_NAME= 'US_VISA'
 COLLECTION_NAME='VISA_DATA'
 MONGODB_URL_KEY='MONGODB_URL'
@@ -17,3 +20,12 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+TARGET_COLUMN:str= "case_status"
+CURRENT_YEAR= date.today().year
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+SCHEMA_FILE_PATH:str=os.path.join("config","schema.yaml")
+
+DATA_VALIDATION_DIR:str ="data_validation"
+DATA_VALIDATION_DRIFT_REPORT_DIR:str="drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str="report.yaml"
--- us_visa/pipline/training_pipeline.py
+++ us_visa/pipline/training_pipeline.py
@@ -2,12 +2,15 @@ import sys
 from us_visa.exception import USvisaException
 from us_visa.logger import logging
 from us_visa.components.data_ingestion import DataIngestion
-from us_visa.entity.config_entity import DataIngestionConfig
-from us_visa.entity.artifact_entity import DataIngestionArtifact
+from us_visa.components.data_validation import DataValidation
+
+from us_visa.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from us_visa.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
+        self.data_validation_config=DataValidationConfig()
         
     def start_data_ingestion(self):
         try:
@@ -22,9 +25,22 @@ class TrainPipeline:
             return data_ingestion_artifact
         except Exception as e:
             raise USvisaException(e,sys)
-        
+    
+    def start_data_validation(self,data_ingestion_artifact):
+        try:
+            data_validation=DataValidation(data_ingestion_artifact,self.data_validation_config)
+            data_validation_artifact= data_validation.initiate_data_validation() 
+            logging.info("Performed the data validation operation")
+
+            logging.info(
+                    "Exited the start_data_validation method of TrainPipeline class"
+                )  
+            return data_validation_artifact
+        except Exception as e:
+            raise USvisaException(e,sys) 
     def run_pipeline(self, ) -> None:
         try:
             data_ingestion_artifact = self.start_data_ingestion()
+            data_validation_artifact=self.start_data_validation(data_ingestion_artifact)
         except Exception as e:
             raise USvisaException(e, sys)
